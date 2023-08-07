06c25b9f49cdbc6c9d089b82f143bba9d8771ec3
muhammadhamza9676 <138454400+muhammadhamza9676@users.noreply.github.com>
2023-08-07 23:28:51 +0500

    Update db.js

--- db.js
+++ db.js
@@ -1,13 +1,18 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 const BASE_URL = process.env.BASE_URL;
+const connectionOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  writeConcern: {
+    w: 'majority' // Valid write concern mode
+  }
+};
 
 
 const connectToMongo = async () => {
     try {
-      const conn = await mongoose.connect(`${BASE_URL}`, {
-        useNewUrlParser: true,
-      });
+      const conn = await mongoose.connect(`${BASE_URL}`, connectionOptions);
       console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
       console.error(error.message);
